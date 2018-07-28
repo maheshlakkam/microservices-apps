531687060738b82f52ad2b916601322b63d13346
maheshlakkam <mahesh.lakkam@gmail.com>
2018-07-29 00:19:08 +0530

    Update NetflixEurekaNamingServiceApplication.java

--- netflix-eureka-naming-service/src/main/java/com/microservice/netflixeurekanamingservice/NetflixEurekaNamingServiceApplication.java
+++ netflix-eureka-naming-service/src/main/java/com/microservice/netflixeurekanamingservice/NetflixEurekaNamingServiceApplication.java
@@ -2,7 +2,7 @@ package com.microservice.netflixeurekanamingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@EnableEurekaServer
 @SpringBootApplication
 public class NetflixEurekaNamingServiceApplication {
 
